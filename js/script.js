8dd723e5ce42de6753979585c0f3f6ef38e02f3c
Allekksa <allekksa@gmail.com>
2022-05-20 19:15:58 +0300

    сверстано бургер меню, доработана пк версия

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-const burger = document.querySelector('.burger-menu');
-const headerNav =  document.querySelector('.header__nav')
-burger.addEventListener('click', () => {
-  headerNav.style.transform = 'translateX(-90%)';
+const burgerOpen = document.querySelector('.burger');
+const burgerClose =  document.querySelector('.menu__close');
+navList = document.querySelector('.nav__list');
+headerBurger = document.querySelector('.header__burger');
+burgerOpen.addEventListener('click', () => {
+  navList.style.display = 'flex';
+  headerBurger.style.display = 'none';
+  burgerClose.style.display = 'block';
+});
+
+burgerClose.addEventListener('click', () => {
+  navList.style.display = 'none';
+  burgerClose.style.display = 'none';
+  burgerOpen.style.display = 'flex';
 })
