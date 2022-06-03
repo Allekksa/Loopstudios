32ac29630920934434c080642bc83d6cfb832dfe
Allekksa <allekksa@gmail.com>
2022-06-03 13:40:47 +0300

    мелкие правки

--- js/script.js
+++ js/script.js
@@ -1,30 +1,27 @@
 const burgerOpen = document.querySelector('.burger');
-const burgerClose =  document.querySelector('.menu__close');
+const burgerClose =  document.querySelector('.burger__close');
 const navList = document.querySelector('.nav__list');
-const headerBurger = document.querySelector('.header__burger');
-const openMenu = document.querySelector('.open__menu');
-const headerContainer = document.querySelector('.header__container')
+const headerContainer = document.querySelector('.header__container');
+const headerMenu = document.querySelector('.header__menu');
+const headerLogo = document.querySelector('.header__logo');
+
+
 burgerOpen.addEventListener('click', () => {
-  navList.style.display = 'flex';
-  headerBurger.style.display = 'none';
-  burgerClose.style.display = 'block';
-  openMenu.style.flexDirection = "column";
-  openMenu.style.justifyContent = "flex-start";
-  headerContainer.classList.add('menu__header');
-  headerContainer.style.display = "flex";
-
-
-
+  burgerOpen.style.display = 'none';
+  burgerClose.style.display = 'flex';
+  navList.classList.add('nav__list--burger');
+  headerContainer.classList.add('open--burger-menu');
+  headerMenu.classList.add('header__burger-menu');
+  headerLogo.style.marginBottom = '160px'
 
 });
 
 burgerClose.addEventListener('click', () => {
-  navList.style.display = 'none';
   burgerClose.style.display = 'none';
   burgerOpen.style.display = 'flex';
-  openMenu.style.flexDirection = "row";
-  openMenu.style.justifyContent = "space-between";
-  openMenu.style.alignItems = "flex-start";
-  headerContainer.style.display = "block";
-  headerContainer.classList.remove('menu__header');
+  navList.classList.remove('nav__list--burger');
+  headerContainer.classList.remove('open--burger-menu');
+  headerMenu.classList.remove('header__burger-menu')
+  headerLogo.style.marginBottom = '0'
+
 })
