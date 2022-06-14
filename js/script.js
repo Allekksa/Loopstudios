6aca1cedb7cd7854002e5f7e6d6b1c45e09f35da
Allekksa <allekksa@gmail.com>
2022-06-14 21:43:13 +0300

    переделано бургер меню

--- js/script.js
+++ js/script.js
@@ -1,27 +1,21 @@
-const burgerOpen = document.querySelector('.burger');
-const burgerClose =  document.querySelector('.burger__close');
-const navList = document.querySelector('.nav__list');
-const headerContainer = document.querySelector('.header__container');
-const headerMenu = document.querySelector('.header__menu');
-const headerLogo = document.querySelector('.header__logo');
+const burgerOpen = document?.querySelector('.burger');
+const burgerClose = document?.querySelector('.burger-close');
+const nav = document?.querySelector('.header__nav');
+const header = document?.querySelector('.header');
 
-
-burgerOpen.addEventListener('click', () => {
+burgerOpen?.addEventListener('click', ()=>{
   burgerOpen.style.display = 'none';
-  burgerClose.style.display = 'flex';
-  navList.classList.add('nav__list--burger');
-  headerContainer.classList.add('open--burger-menu');
-  headerMenu.classList.add('header__burger-menu');
-  headerLogo.style.marginBottom = '160px'
+  burgerClose.style.display = 'block';
+  nav.style.display = 'flex';
+  header.classList.add('menu--visible');
+})
 
-});
-
-burgerClose.addEventListener('click', () => {
+burgerClose?.addEventListener('click', ()=>{
+  burgerOpen.style.display = 'block';
   burgerClose.style.display = 'none';
-  burgerOpen.style.display = 'flex';
-  navList.classList.remove('nav__list--burger');
-  headerContainer.classList.remove('open--burger-menu');
-  headerMenu.classList.remove('header__burger-menu')
-  headerLogo.style.marginBottom = '0'
+  nav.style.display = 'none';
+  header.classList.remove('menu--visible');
 
+  
+  
 })
