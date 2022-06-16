88bb0a255b59f4e4e94aa7df7e89e1f40acecc1f
Allekksa <allekksa@gmail.com>
2022-06-16 12:51:30 +0300

    переделан бургер, убрана лишняя кнопка

--- js/script.js
+++ js/script.js
@@ -1,21 +1,14 @@
-const burgerOpen = document?.querySelector('.burger');
-const burgerClose = document?.querySelector('.burger-close');
+const burger = document?.querySelector('.burger');
+const menu = document?.querySelector('.burger-menu');
 const nav = document?.querySelector('.header__nav');
 const header = document?.querySelector('.header');
-
-burgerOpen?.addEventListener('click', ()=>{
-  burgerOpen.style.display = 'none';
-  burgerClose.style.display = 'block';
-  nav.style.display = 'flex';
-  header.classList.add('menu--visible');
-})
-
-burgerClose?.addEventListener('click', ()=>{
-  burgerOpen.style.display = 'block';
-  burgerClose.style.display = 'none';
-  nav.style.display = 'none';
-  header.classList.remove('menu--visible');
-
+const body = document?.querySelector('body');
+burger?.addEventListener('click', ()=>{
   
-  
+  nav?.classList.toggle('nav--active');
+  header?.classList.toggle('menu--visible');
+  menu?.classList.toggle('burger-menu--active');
+  burger?.classList.toggle('burger--active');
+  body?.classList.toggle('stop-scroll');
+
 })
